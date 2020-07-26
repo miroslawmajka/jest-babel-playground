61750dd25898f92ad4be1ab6051f67d66835cfbe
miroslawmajka <go2mirek@gmail.com>
2020-07-26 20:45:57 +0100

    Fixed numbers in output

--- app.js
+++
@@ -1,5 +0,0 @@
-import { sum } from './src/math';
-
-const result = sum(2, 5);
-
-console.log(`Adding 2 and 3 gives you ${result}`);
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import { sum } from './math';
+
+const numberOne = 2;
+const numberTwo = 5;
+
+const result = sum(numberOne, numberTwo);
+
+console.log(`Adding ${numberOne} and ${numberTwo} gives you ${result}`);
