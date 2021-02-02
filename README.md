2de2560eb3fedf1ec52b4cfa08fb3e7e433b7bf9
miroslawmajka <go2mirek@gmail.com>
2021-02-02 21:23:23 +0000

    Simplified dependencies

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 [![Build Status](https://dev.azure.com/miroslawmajka/jest-babel-playground/_apis/build/status/miroslawmajka.jest-babel-playground?branchName=master)](https://dev.azure.com/miroslawmajka/jest-babel-playground/_build/latest?definitionId=3&branchName=master)
 
 This is a simple pipeline project with the intention of learning to use Jest, Babel and next generation JavaScript in Node.
+
+```
+npm install
+npm start
+npm test
+```
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'babel',
-    coverageReporters: ['cobertura', 'text', 'lcov', 'clover'],
-    reporters: ['default', 'jest-junit'],
-    testEnvironment: 'node'
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  coverageReporters: ['cobertura', 'text', 'lcov', 'clover'],
+  reporters: ['default', 'jest-junit'],
+  testEnvironment: 'node',
 };
